bec577380f05f832dc17cb2638c37bec06b2c970
HenryHill505 <hhill505@gmail.com>
2018-07-11 09:37:48 -0500

    Added field randomizer to Sweepstakes and used it to write logic for PickWinner()

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -10,16 +10,21 @@ namespace Sweepstakes
     {
         string name;
         Dictionary<int, Contestant> contestants;
+        Random randomizer;
 
         public Sweepstakes(string name)
         {
             this.name = name;
             contestants = new Dictionary<int, Contestant >();
+            randomizer = new Random();
         }
 
         public string PickWinner()
         {
-
+            int contestantCount = contestants.Count;
+            int winnerRegistrationNumber = randomizer.Next(1, contestantCount + 1);
+            string winnerName = $"{contestants[winnerRegistrationNumber].firstName} {contestants[winnerRegistrationNumber].lastName}";
+            return winnerName;
         }
 
         public void PrintContestantInfo(Contestant contestant)
